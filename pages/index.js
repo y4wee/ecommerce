b0efafefc8d04287e16d39f51adc9e2b6474df80
y4wee <quentin.jambert@gmail.com>
2022-08-24 17:57:52 +0200

    footerBanner component

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const Home = ({ products, bannerData }) => {
                 ))}
             </div>
 
-            <FooterBanner />
+            <FooterBanner footerBanner={bannerData.length && bannerData[0]}/>
         </div>
     );
 };
