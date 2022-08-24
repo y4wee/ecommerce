0f3dad0237c7632a73215997610f80922a39f22f
y4wee <quentin.jambert@gmail.com>
2022-08-24 15:43:10 +0200

    creation components and connexion to api

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FooterBanner, HeroBanner, Product } from "../components";
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
+const Home = () => {
+    return (
+        <div>
+            <HeroBanner />
 
-export default index
+            <div className="products-heading">
+                <h2>Best Selling Products</h2>
+                <p>Speakers of many variations</p>
+            </div>
+
+            <div className="products-container">
+                {["Product1", "Product2"].map((product) => product)}
+            </div>
+
+            <FooterBanner />
+            
+        </div>
+    );
+};
+
+export default Home;
